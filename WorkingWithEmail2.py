620209f159a07c6e23190dddfd28c55dd3f397df
Peter <osztodipeter@gmail.com>
2019-01-07 13:01:24 +0000

    mail upgrade to multi attachment

--- WorkingWithEmail2.py
+++ WorkingWithEmail2.py
@@ -22,28 +22,32 @@ try:
     msg['From'] = sender_email
     msg['To'] = receiver_email
 
-    filebasename = 'Moai_Rano_raraku.jpg'
-    filepath = "/home/peter/Documents/"
-    filefullname = filepath + filebasename
-    attachment = open(filefullname,'rb')
+    fullpath = []
+    #path = r'/home/peter/Documents/' # there was a problem with the os library, it could execute the listdir method
+    fullpath.append('/home/peter/Documents/mail_')
+    fullpath.append('/home/peter/Documents/Moai_Rano_raraku.jpg')
+
+    for x in fullpath:
+        attachment = open(x,'rb')
+        part0 = MIMEBase('application', 'octet-stream')
+        part0.set_payload(attachment.read())
+        encoders.encode_base64(part0)
+        part0.add_header('Content-Disposition', 'attachement ; filename = ' + x.split("/")[-1])
+        msg.attach(part0)
 
     plain_text = 'Part 1 : This is the plain text part of the email. Check out the attachment I sent. It could not only be a document but picture or other files.'
     html_text = '<p> <b> Part 2 :</b> <br>  This is the html part of the email. </p>'
 
-    part0 = MIMEBase('application','octet-stream')
-    part0.set_payload(attachment.read())
-    encoders.encode_base64(part0)
-    part0.add_header('Content-Disposition','attachement ; filename = '+ filebasename)
-
     part1 = MIMEText(plain_text, 'plain')
     part2 = MIMEText(html_text, 'html')
 
-    msg.attach(part0)
     msg.attach(part1)
     msg.attach(part2)
 
     smtp_server.sendmail(from_addr=sender_email,to_addrs=receiver_email, msg=msg.as_string())
     smtp_server.quit()
+
 except smtplib.SMTPException as e:
     print(e)
+
 
