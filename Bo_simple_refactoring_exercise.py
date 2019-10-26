f84f0f9f184ddf5b589ecb45a681a4954c2cfe4f
Peter <osztodipeter@gmail.com>
2019-10-26 21:35:36 +0100

    unittest fix

--- Bo_simple_refactoring_exercise.py
+++ Bo_simple_refactoring_exercise.py
@@ -4,12 +4,21 @@ from log_parser import return_method
 
 class TestLogParser(unittest.TestCase):
 
+    def setUp(self):
+        print('setUp log line')
+        self.log_line = '10.0.0.208 - - [17/Jul/2018:15:40:16] /v1/escalation/predict 200 Pingdom.com_bot_version_1.4'
+
+    def tearDown(self):
+        print('tearDown \n')
+
     def test_parse(self):
-        values = return_method('10.0.0.208 - - [17/Jul/2018:15:40:16] /v1/escalation/predict 200 Pingdom.com_bot_version_1.4', 'log')
+        print('Log test')
+        values = return_method(self.log_line, 'log')
         self.assertEqual(values[0:5], ['10.0.0.208', '17/Jul/2018:15:40:16', '/v1/escalation/predict', '200', 'Pingdom.com_bot_version_1.4'])
 
     def test_csv(self):
-        values = return_method('10.0.0.208 - - [17/Jul/2018:15:40:16] /v1/escalation/predict 200 Pingdom.com_bot_version_1.4', 'csv')
+        print('Csv test')
+        values = return_method(self.log_line, 'csv')
         self.assertEqual(values, '10.0.0.208,17/Jul/2018:15:40:16,/v1/escalation/predict,200,Pingdom.com_bot_version_1.4')
 
 
